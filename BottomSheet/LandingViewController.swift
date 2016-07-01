ab3b45c3b1d75c51ef4f8a05bd4b4967ab0d0703
Ahmed Elassuty <ahmed.elassuty214@gmail.com>
2016-07-01 21:43:37 +0200

    Add colored squares to show blur and vibrancy effect

--- BottomSheet/LandingViewController.swift
+++ BottomSheet/LandingViewController.swift
@@ -37,14 +37,5 @@ class LandingViewController: UIViewController {
         let width  = view.frame.width
         bottomSheetVC.view.frame = CGRectMake(0, self.view.frame.maxY, width, height)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
